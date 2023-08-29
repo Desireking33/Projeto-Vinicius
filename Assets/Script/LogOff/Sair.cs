e0327b43533d90097c4dccc390941cb1f82935dc
Desireking33 <143457655+Desireking33@users.noreply.github.com>
2023-08-28 21:40:51 -0300

    Add files via upload

---
+++ Assets/Script/LogOff/Sair.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sair : MonoBehaviour
+{
+   public void LogOff(){
+    Application.Quit(0);
+    SceneManager.LoadScene("Cena1");
+
+
+   }
+}
---
+++ Assets/Script/Login/Logar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Logar : MonoBehaviour
+{
+    public void Login(){
+        SceneManager.LoadScene("Cena2");
+    }
+    
+}
+
